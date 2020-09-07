dab96180f2140903bce9323a7f338be7c263026c
wildenali <wildenwildenaliali@gmail.com>
2020-09-07 11:19:01 +0700

    Insert documents

---
+++ scripts/insert.js
@@ -0,0 +1,37 @@
+// Insert document customers
+db.customers.insertOne({
+  _id: "Khannedy",
+  name: "Eko Kurniawan",
+});
+
+// Insert document products
+db.productsinserMany([
+  {
+    _id: 1,
+    name: "Indomie Ayam Bawang",
+    price: new NumberLong(2000),
+  },
+  {
+    _id: 2,
+    name: "Mie Sedap Soto",
+    price: new NumberLong(1800),
+  },
+]);
+
+// Insert document orders
+db.orders.insertOne({
+  _id: new ObjectId(),
+  total: new NumberLong(8000),
+  item: [
+    {
+      produk_id: 1,
+      price: new NumberLong(2000),
+      quantity: new NumberInt(2),
+    },
+    {
+      produk_id: 2,
+      price: new NumberLong(2000),
+      quantity: new NumberInt(2),
+    },
+  ],
+});
