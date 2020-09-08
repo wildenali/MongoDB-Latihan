683cff3a127ad3d1b6732ce5008a890685f303be
wildenali <wildenwildenaliali@gmail.com>
2020-09-08 14:29:49 +0700

    Query Modifier

--- scripts/queryModifier.js
+++ scripts/queryModifier.js
@@ -12,19 +12,19 @@ sort(query) -> Mengurutkan hasil data query
 */
 
 // select count(*) from products
-db.products.fin({}).count();
+db.products.find({}).count();
 
 // select * from products limit 4
-db.products.fin({}).limit(4);
+db.products.find({}).limit(4);
 
 // select * from products offset 2
-db.products.fin({}).skip(2);
+db.products.find({}).skip(2);
 
 // select * from products limit 4 offset 2
-db.products.fin({}).limit(4).skip(2);
+db.products.find({}).limit(4).skip(2);
 
 // select * from products order by name asc, category desc
-db.products.fin({}).sort({
+db.products.find({}).sort({
   name: 1,
   category: -1,
 });
